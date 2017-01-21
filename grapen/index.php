1d72a9c5e8be4c23766fcd74980d55990f48603a
Siketyan <siketyan@sikeserver.com>
2017-01-21 11:32:47 +0900

    下からうにょんを実現

---
+++ grapen/index.php
@@ -0,0 +1,36 @@
+<?php include "header.php"; ?>
+      <div id="intro" align="center" style="padding:50px 20px;background-color:#f0f0f0">
+        <img src="img/logo.png" Width=150>
+        <h1 style="margin-top:10px">GrapeN</h1>
+        <h3>Customize everywhere, Tweet everywhere.</h3>
+        <br>
+        <a href="//dl.sikeserver.com/pub/GrapeN/latest" target="_blank"
+           class="btn btn-success btn-lg btn-success-border">
+          &nbsp;<i class="fa fa-download fa-fw"></i> 今すぐダウンロード&nbsp;
+        </a>
+        <br><br>
+      </div>
+      <div class="container">
+        <br><br>
+        <div class="row">
+          <div class="col-md-4" align="center">
+            <h1 style="font-size:150px"><i class="fa fa-paint-brush"></i></h1>
+            <h2>フルカスタマイズ</h2>
+            <p class="lead">GrapeNを使えば、ユーザーインターフェースのありとあらゆる場所まで自由自在。一つのファイルで好みのデザインを交換できます。</p>
+          </div>
+          <div class="col-md-4" align="center">
+            <h1 style="font-size:150px"><i class="fa fa-cloud"></i></h1>
+            <h2>軽量で多機能</h2>
+            <p class="lead">GrapeNは、他の作業を邪魔しません。リソースの消費を抑えながらも、多機能なクライアントを実現しました。</p>
+          </div>
+          <div class="col-md-4" align="center">
+            <h1 style="font-size:150px"><i class="fa fa-refresh"></i></h1>
+            <h2>重なるアップデート</h2>
+            <p class="lead">GrapeNは、アップデートによって日々進化します。機能追加などを随時アップデートとしてリリースし、最高のクライアントを作り上げていきます。</p>
+          </div>
+        </div>
+        <br><hr>
+        <!--<h3>リアルタイム。</h3>-->
+        <p class="lead"></p>
+      </div>
+<?php include "footer.php"; ?>
